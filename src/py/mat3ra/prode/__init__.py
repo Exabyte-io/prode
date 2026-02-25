cb79e58bd75ed05920e7614ae02029a172ce9fac
VsevolodX <vsevolodbiryukov@icloud.com>
2026-02-24 17:28:02 -0800

    update: use ESSE

--- src/py/mat3ra/prode/__init__.py
+++ src/py/mat3ra/prode/__init__.py
@@ -4,38 +4,38 @@ from types import SimpleNamespace
 from mat3ra.esse.models.properties_directory.enum_options import (
     ExternalSource,
     PropertyType,
-    ScalarPropertyName,
-    NonScalarPropertyName,
-    TensorPropertyName,
-    ObjectPropertyName,
-    ProtoPropertyName,
-    MetaPropertyName,
+    ScalarPropertyEnum,
+    NonScalarPropertyEnum,
+    TensorPropertyEnum,
+    ObjectPropertyEnum,
+    ProtoPropertyEnum,
+    MetaPropertyEnum,
 )
 
 
-class AllPropertyName(str, Enum):
+class AllPropertyEnum(str, Enum):
     """All property names combined from all categories"""
     pass
 
 
 for enum_class in [
-    ScalarPropertyName,
-    NonScalarPropertyName,
-    TensorPropertyName,
-    ObjectPropertyName,
-    ProtoPropertyName,
-    MetaPropertyName,
+    ScalarPropertyEnum,
+    NonScalarPropertyEnum,
+    TensorPropertyEnum,
+    ObjectPropertyEnum,
+    ProtoPropertyEnum,
+    MetaPropertyEnum,
 ]:
     for item in enum_class:
-        setattr(AllPropertyName, item.name, item.value)
+        setattr(AllPropertyEnum, item.name, item.value)
 
 
 PropertyName = SimpleNamespace(
-    scalar=ScalarPropertyName,
-    non_scalar=NonScalarPropertyName,
-    tensor=TensorPropertyName,
-    object=ObjectPropertyName,
-    proto=ProtoPropertyName,
-    meta=MetaPropertyName,
-    all=AllPropertyName,
+    scalar=ScalarPropertyEnum,
+    non_scalar=NonScalarPropertyEnum,
+    tensor=TensorPropertyEnum,
+    object=ObjectPropertyEnum,
+    proto=ProtoPropertyEnum,
+    meta=MetaPropertyEnum,
+    all=AllPropertyEnum,
 )
