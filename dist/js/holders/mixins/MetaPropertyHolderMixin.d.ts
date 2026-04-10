7f91264cc939417ce332c42de103759c900351c3
Kostiantyn Dvornik <dvornik.konstantin@gmail.com>
2026-04-10 15:23:41 +0300

    chore: improve types

--- dist/js/holders/mixins/MetaPropertyHolderMixin.d.ts
+++ dist/js/holders/mixins/MetaPropertyHolderMixin.d.ts
@@ -1,12 +1,12 @@
 import type { InMemoryEntity } from "@mat3ra/code/dist/js/entity";
 import type { AnyObject } from "@mat3ra/esse/dist/js/esse/types";
 import type { MetaPropertyHolderSchema } from "@mat3ra/esse/dist/js/types";
+import type { PseudopotentialMetaProperty } from "src/js";
 import { type MetaPropertyHolderSchemaMixin } from "../../generated/MetaPropertyHolderSchemaMixin";
-import type MetaProperty from "../../MetaProperty";
 export interface MetaPropertySchemaJSON extends MetaPropertyHolderSchema, AnyObject {
 }
 export type MetaPropertyHolderMixin = {
-    property: MetaProperty;
+    property: PseudopotentialMetaProperty;
 } & MetaPropertyHolderSchemaMixin;
 export type MetaPropertyInMemoryEntity = InMemoryEntity & MetaPropertyHolderMixin;
 export declare function metaPropertyHolderMixin(item: InMemoryEntity): void;
